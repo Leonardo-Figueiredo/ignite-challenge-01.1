f1100da61580e695140d32782be2cae58dd2c3af
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2021-04-29 10:21:47 -0300

    feat(content): created Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
-  // Complete aqui
+import { Genre } from "../entities/Genre"
+import { Movie } from "../entities/Movie"
+import { Header } from './Header'
+import { MovieCard } from './MovieCard'
+
+interface ContentProps {
+	selectedGenre: Genre
+	movies: Movie[]
 }
+
+const Content: React.FC<ContentProps> = ({ selectedGenre, movies }) => {
+	return (
+		<div className="container">
+			<Header genreTitle={selectedGenre.title} />
+
+			<main>
+				<div className="movies-list">
+					{movies.map(movie => (
+						<MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+					))}
+				</div>
+			</main>
+		</div>
+	)
+}
+
+export { Content }
